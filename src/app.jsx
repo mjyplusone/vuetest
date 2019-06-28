ee7e8cb59a5067b834ddb5d230948b2b71c79cae
mjyplusone <1546331221@qq.com>
2019-06-28 11:49:41 +0800

    update

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+// let a = 123;
+// let test = (a) => {
+//     return a * 2;
+// }
+// test(a);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(
+    <h1>Hello World!</h1>,
+    document.getElementById('app')
+); 
